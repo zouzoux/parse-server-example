2cdcbbd029f091e705100fa9a5eeb039aa4bd9f5
zouzoux <zouzoux4@gmail.com>
2017-01-19 20:47:31 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -8,8 +8,8 @@
 
 Parse.serverURL = 'https://comerate2016.herokuapp.com/parse/';
 
-
-var j = schedule.scheduleJob(' */1 * * * *', function(){
+var j = require('node-schedule');
+ j = schedule.scheduleJob(' */1 * * * *', function(){
   console.log('The answer to life, the universe, and everything!');
 	 var userQuery = new Parse.Query('_User');
 	userQuery.limit(1000);
